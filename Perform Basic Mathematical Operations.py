fcfb22616d9bc2bb7ec55f057653bb815acb9a8b
kteotia5733-hash <kteotia5733@gmail.com>
2026-02-22 18:06:07 +0530

    Add files via upload

---
+++ Perform Basic Mathematical Operations.py
@@ -0,0 +1,16 @@
+first = input("Enter the First Number")
+Second = input("Enter the Second Number")
+
+
+#basic mathematical operations
+
+Addition = int(first) + int(Second)
+Subtraction = int(first) - int(Second)
+Multiplication = int(first) * int(Second)
+Division = float(first) / float(Second)
+
+print("\n\n")
+print("Addition :", Addition)
+print("Subtraction :", Subtraction)
+print("Multiplication :", Multiplication)
+print("Division :", Division)
